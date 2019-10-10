459610e4622ec9ae6344873aab9e25b267ef7fed
Jungs <jungs6734@gmail.com>
2019-10-10 22:11:57 +0900

    Map Asset 추가 및 캐릭터 애니메이션 제작ing

---
+++ AbilityRPG/Assets/Scripts/Character/PlayerController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float Speed = 2.0f;         // 움직이는 스피드.
+    
+    protected Joystick joystick;
+    protected Joybutton joybutton;
+
+    private Rigidbody rigidbody;
+    private Animator animator;
+
+    void Start()
+    {
+        joystick = FindObjectOfType<Joystick>();
+        joybutton = FindObjectOfType<Joybutton>();
+
+        rigidbody = GetComponent<Rigidbody>();
+        animator = GetComponent<Animator>();
+    }
+
+
+    public void FixedUpdate()
+    {
+        animator.SetBool("MOVE", false);
+
+        if (joystick.Vertical != 0 || joystick.Horizontal != 0)
+        {
+            animator.SetBool("MOVE", true);
+
+            transform.Translate(Vector3.forward * Speed * Time.deltaTime);
+            transform.rotation = Quaternion.Euler(0f, Mathf.Atan2(joystick.Horizontal, joystick.Vertical) * Mathf.Rad2Deg, 0f);
+        }
+    }
+}
--- AbilityRPG/Assets/Scripts/Character/PlayerTargeting.cs
+++ AbilityRPG/Assets/Scripts/Character/PlayerTargeting.cs
@@ -4,17 +4,21 @@ using UnityEngine;
 
 public class PlayerTargeting : MonoBehaviour
 {
+    // 해당 스테이지에 있는 몬스터를 받아올 리스트
     public List<GameObject> monsterList;
 
     float currentDist = 0f;      // 현재거리
-    float closetDist = 10f;     // 가까운 거리
-    float targetDist = 10f;     // 타겟 거리
+    float closetDist = 100f;     // 가까운 거리
+    float targetDist = 100f;     // 타겟 거리
 
     int closetDistIndex = 0;    // 가장 가까운 인덱스
     int targetIndex = -1;       // 타겟 할 인덱스
 
+    public bool getTarget = false;
+
     public LayerMask layerMask;
 
+    // 시각적 표현 (에디터에서)
     private void OnDrawGizmos()
     {
         for (int i = 0; i < monsterList.Count; i++)
@@ -51,9 +55,34 @@ public class PlayerTargeting : MonoBehaviour
                 bool isHit = Physics.Raycast(transform.position, monsterList[i].transform.position - transform.position,
                     out hit, 20f, layerMask);
 
+                // 레이저에 맞은 타겟중 장애물에 안걸린 몬스터
                 if (isHit && hit.transform.CompareTag("Monster"))
-                    Debug.Log(hit.transform.name);
+                {
+                    if (targetDist >= currentDist)
+                    {
+                        targetIndex = i;
+                        targetDist = currentDist;
+                    }
+                }
+
+                // 장애물에 상관없이 가까운 몬스터
+                if (closetDist >= currentDist)
+                {
+                    closetDistIndex = i;
+                    closetDist = currentDist;
+                }
             }
+
+            // 레이저에 맞은 몬스터가 없으면 가장 가까운 몬스터를 향해 이동
+            if (targetIndex == -1)
+            {
+                targetIndex = closetDistIndex;
+            }
+
+            // 초기화
+            closetDist = 100f;
+            targetDist = 100f;
+            getTarget = true;
         }
     }
 }
